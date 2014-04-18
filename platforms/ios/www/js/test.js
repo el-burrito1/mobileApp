3bcfe68215ff30ea9439dca8ee53ce0e2aa8561c
Spencer Spiegel <spencer@Spencers-MacBook-Pro.local>
2014-04-18 11:00:47 -0700

    changes

--- platforms/ios/www/js/test.js
+++ platforms/ios/www/js/test.js
@@ -1,20 +1,26 @@
 $(document).on('ready' , function(){
 
-	// function initialize() {
-	//   var mapOptions = {
-	//     center: new google.maps.LatLng(-34.397, 150.644),
-	//     zoom: 8
-	//   };
-	//   var map = new google.maps.Map(document.getElementById("map-canvas"),
-	//       mapOptions);
-	// }
-	// google.maps.event.addDomListener(window, 'load', initialize);
-
 	function onDeviceReady() {
        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
 
     function onSuccess(position){
+
+    	$.ajax({
+    		type: 'GET',
+    		url:"http://127.0.0.1:3000/endpoint",
+    		data: {latitude:position.coords.latitude , longitude: position.coords.longitude},
+    		dataType: 'jsonp',
+    		contentType: 'application/json',
+    		crossDomain: true,
+    		success: function(data){
+    			console.log('data successfully sent');
+    		},
+    		error: function(){
+    			console.log('there was an error');
+    		}
+    	});
+
     	console.log(position.coords.longitude);
     	console.log(position.coords.latitude);
     	function initialize() {
@@ -33,8 +39,6 @@ $(document).on('ready' , function(){
     }
 
     onDeviceReady();
-
-
 
 
 })
